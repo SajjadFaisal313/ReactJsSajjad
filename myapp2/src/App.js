7a5a22d58b4a49d5bcba7721488de4eb64e199f2
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-19 13:34:45 +0530

    myapp2

--- myapp2/src/App.js
+++ myapp2/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {Sections} from "./Components/Sections";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sections/>
     </div>
   );
 }
---
+++ myapp2/src/Components/Sections.jsx
@@ -0,0 +1,24 @@
+import "../Components/Sections.css";
+export const Sections=() => {
+    return(
+        <div>
+        <section>
+            <h3>HTML</h3>
+            <ul>efgsfgtr</ul>
+            <ul>fgsfgrdf</ul>
+            <ul>gfsfdgafw</ul>
+        </section>
+        <section>
+            <h3>HTML</h3>
+            <ul>efgsfgtr</ul>
+            <ul>fgsfgrdf</ul>
+            <ul>gfsfdgafw</ul>
+        </section>        <section>
+            <h3>HTML</h3>
+            <ul>efgsfgtr</ul>
+            <ul>fgsfgrdf</ul>
+            <ul>gfsfdgafw</ul>
+        </section>
+        </div>
+    )
+}
