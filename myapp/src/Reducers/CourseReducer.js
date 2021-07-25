8bedec0a97bdedf2724871eacdea27aad71551ae
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-25 12:51:35 +0530

    Create CourseReducer.js

---
+++ myapp/src/Reducers/CourseReducer.js
@@ -0,0 +1,33 @@
+export const CoursesReducer = {
+    Data :[
+        {
+            courseName: "DAC",
+            instituteName: "C-DAC",
+            contact: "645465646",
+            city: "Nanded",
+        }
+        {
+            courseName: "Networking",
+            instituteName: "Seed Info",
+            contact: "954378384",
+            city: "Mumbai",
+        }
+        {
+            courseName: "hTML",
+            instituteName: "A-DAC",
+            contact: "675767698",
+            city: "Nanded",
+        }
+        {
+            courseName: "React",
+            instituteName: "k-DAC",
+            contact: "4567867684",
+            city: "Nanded",
+        }
+        {
+            courseName: "CSS",
+            instituteName: "i-DAC",
+            contact: "65896866",
+            city: "Nanded",
+        }
+    ]
