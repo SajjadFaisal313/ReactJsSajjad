8f61c5b4bfd2e93f7156d2caf3478c9d49c61633
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-26 16:06:20 +0530

    Update FormTable.jsx

--- myapp/src/components/FormTable.jsx
+++ myapp/src/components/FormTable.jsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import {Row ,Col, Form, Button} from "react-bootstrap";
+import React from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
 
 export const FormTable = () => {
-    return (
-        <div>
-        <Row>
+  return (
+    <div>
+      <Row>
         <Col>
-        <Form.Control type="text" placeholder="First Name">
-        </Form.Control>
+          <Form.Control type="text" placeholder="First Name"></Form.Control>
         </Col>
         <Col>
-        <Form.Control type="text" placeholder="Middle Name">
-        </Form.Control>
+          <Form.Control type="text" placeholder="Middle Name"></Form.Control>
         </Col>
         <Col>
-        <Form.Control type="text" placeholder="Last Name">
-        </Form.Control>
+          <Form.Control type="text" placeholder="Last Name"></Form.Control>
         </Col>
-    </Row>
-    <Button>Submit</Button>
-    <Row>
+      </Row>
+      <Button>Submit</Button>
+      <Row>
         <Col>
-        <Form.Control type="text" placeholder="Email">
-        </Form.Control>
+          <Form.Control type="text" placeholder="Email"></Form.Control>
         </Col>
+        <Col></Col>
         <Col>
-        <Form.Control type="text" placeholder="Phone Number">
-        </Form.Control>
+          <Form.Control type="text" placeholder="Phone Number"></Form.Control>
         </Col>
-    </Row>
-    <Row>
+      </Row>
+      <Row>
         <Col>
-        <Form.Control type="text" placeholder="Current Address">
-        </Form.Control>
+          <Form.Control
+            type="text"
+            placeholder="Current Address"
+          ></Form.Control>
         </Col>
-    </Row>
-    <Row>
+      </Row>
+      <Row>
         <Col>
-        <Form.Control type="text" placeholder="Permanant Address">
-        </Form.Control>
+          <Form.Control
+            type="text"
+            placeholder="Permanant Address"
+          ></Form.Control>
         </Col>
-    </Row>
-</div>
-    );
+      </Row>
+    </div>
+  );
 };
