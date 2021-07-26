dcc73be14c56bc0607c478eb022e6a85837660ca
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-26 12:02:58 +0530

    h

---
+++ myapp/src/components/CourseInfoParent.jsx
@@ -0,0 +1,23 @@
+    import FormImpl from "react-bootstrap/esm/Form";
+import {CourseInfo} from "./components/CourseInfo";
+    
+    export {CourseInfoParent} from 
+    <div className="App">
+       <h1>List Of Institutions</h1>
+       <tr>
+     <td><CourseInfo CourseName="B.E (Mech)" Institute="SGGS" City="Nanded" Contact="8956487481"/></td> 
+     <td><CourseInfo CourseName="B.E (Civil)" Institute="DY Patil" City="Pune" Contact="9745658986"/></td>  
+     <td><CourseInfo CourseName="B.E (Electrical)" Institute="GECA" City="Aurangabad" Contact="9356446645"/></td> 
+     </tr>
+      <tr>
+     <td><CourseInfo CourseName="B.E (Mech)" Institute="SGGS" City="Nanded" Contact="8956487481"/></td> 
+     <td><CourseInfo CourseName="B.E (Civil)" Institute="DY Patil" City="Pune" Contact="9745658986"/></td>  
+     <td><CourseInfo CourseName="B.E (Electrical)" Institute="GECA" City="Aurangabad" Contact="9356446645"/></td> 
+     </tr>
+      <tr>
+     <td><CourseInfo CourseName="B.E (Mech)" Institute="SGGS" City="Nanded" Contact="8956487481"/></td> 
+     <td><CourseInfo CourseName="B.E (Civil)" Institute="DY Patil" City="Pune" Contact="9745658986"/></td>  
+     <td><CourseInfo CourseName="B.E (Electrical)" Institute="GECA" City="Aurangabad" Contact="9356446645"/></td> 
+     </tr>
+      <MyInfo />
+    </div>
