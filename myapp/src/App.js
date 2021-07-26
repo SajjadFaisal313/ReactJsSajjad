65add72df351940e2fbe7acd9ef16b6168457da6
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-26 12:18:43 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import {CourseInfoParent} from "./components/CourseInfoParent";
+// import {CourseInfoParent} from "./components/CourseInfoParent";
 // import {MyInfo} from "./components/MyInfo"
 // import {Counter} from "./components/Counter"
 
 function App() {
   return (
-<CourseInfoParent/>
-{/* <Counter/> */}
+
   );
 }
 
