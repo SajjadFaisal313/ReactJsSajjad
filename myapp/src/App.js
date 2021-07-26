a0756dc4bfb2be6bd6d83c185fafb4c6c301b7fc
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-26 12:32:03 +0530

    rg

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-// import {CourseInfoParent} from "./components/CourseInfoParent";
-// import {MyInfo} from "./components/MyInfo"
-// import {Counter} from "./components/Counter"
 
 function App() {
   return (
-
+    
   );
 }
 
