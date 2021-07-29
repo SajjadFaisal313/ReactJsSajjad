58bf11d51e5801b0e1d55c0335a62911f679dae1
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-29 16:06:29 +0530

    Update Piano.jsx

--- myapp/src/components/Piano.jsx
+++ myapp/src/components/Piano.jsx
@@ -4,41 +4,91 @@ import "./Piano.css";
 
 export const Piano = () => {
   const handlePlay = (bit) => {
-  const audio = new Audio(`Pianosounds/${bit}.mp3`);
-  audio.play();
+    const audio = new Audio(`Pianosounds/${bit}.mp3`);
+    audio.play();
   };
 
-  document.addEventListener("keydown")
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "a" && handlePlay("A"));
+    if (e.key === "a" && handlePlay("B"));
+    if (e.key === "a" && handlePlay("C"));
+    if (e.key === "a" && handlePlay("D"));
+    if (e.key === "a" && handlePlay("E"));
+    if (e.key === "a" && handlePlay("F"));
+    if (e.key === "a" && handlePlay("G"));
+  });
   return (
     <div>
       <Row>
         {/* <Col md={1}></Col> */}
         <Col>
-          <Button className="sa" onMouseDown={() => handlePlay("A")}>Sa</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("B")}>Re</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("C")}>Ga</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("D")}>Ma</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("E")}>Pa</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("F")}>Dh</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("G")}>Ni</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("A")}>
+            Sa
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("B")}>
+            Re
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("C")}>
+            Ga
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("D")}>
+            Ma
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("E")}>
+            Pa
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("F")}>
+            Dh
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("G")}>
+            Ni
+          </Button>
         </Col>
         <Col>
-          <Button className="sa" onMouseDown={() => handlePlay("A")}>Sa</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("B")}>Re</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("C")}>Ga</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("D")}>Ma</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("E")}>Pa</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("F")}>Dh</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("G")}>Ni</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("A")}>
+            Sa
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("B")}>
+            Re
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("C")}>
+            Ga
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("D")}>
+            Ma
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("E")}>
+            Pa
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("F")}>
+            Dh
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("G")}>
+            Ni
+          </Button>
         </Col>
         <Col>
-          <Button className="sa" onMouseDown={() => handlePlay("A")}>Sa</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("B")}>Re</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("C")}>Ga</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("D")}>Ma</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("E")}>Pa</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("F")}>Dh</Button>
-          <Button className="sa" onMouseDown={() => handlePlay("G")}>Ni</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("A")}>
+            Sa
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("B")}>
+            Re
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("C")}>
+            Ga
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("D")}>
+            Ma
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("E")}>
+            Pa
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("F")}>
+            Dh
+          </Button>
+          <Button className="sa" onMouseDown={() => handlePlay("G")}>
+            Ni
+          </Button>
         </Col>
         {/* <Col md={1}></Col> */}
       </Row>
