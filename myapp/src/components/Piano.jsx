91a76dc324dc0a726203791e37a38eb745cf7726
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-29 13:13:35 +0530

    Update Piano.jsx

--- myapp/src/components/Piano.jsx
+++ myapp/src/components/Piano.jsx
@@ -3,38 +3,42 @@ import { Button, Row, Col } from "react-bootstrap";
 import "./Piano.css";
 
 export const Piano = () => {
+  const handlePlay = (bit) => {
+  const audio = new Audio(`Pianosounds/${bit}.mp3`);
+  audio.play();
+  };
   return (
     <div>
       <Row>
-        <Col md={1}></Col>
+        {/* <Col md={1}></Col> */}
         <Col>
-          <Button>Sa</Button>
-          <Button>Re</Button>
-          <Button>Ga</Button>
-          <Button>Ma</Button>
-          <Button>Pa</Button>
-          <Button>Dh</Button>
-          <Button>Ni</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("A")}>Sa</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("B")}>Re</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("C")}>Ga</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("D")}>Ma</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("E")}>Pa</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("F")}>Dh</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("G")}>Ni</Button>
         </Col>
         <Col>
-          <Button>Sa</Button>
-          <Button>Re</Button>
-          <Button>Ga</Button>
-          <Button>Ma</Button>
-          <Button>Pa</Button>
-          <Button>Dh</Button>
-          <Button>Ni</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("A")}>Sa</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("B")}>Re</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("C")}>Ga</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("D")}>Ma</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("E")}>Pa</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("F")}>Dh</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("G")}>Ni</Button>
         </Col>
         <Col>
-          <Button>Sa</Button>
-          <Button>Re</Button>
-          <Button>Ga</Button>
-          <Button>Ma</Button>
-          <Button>Pa</Button>
-          <Button>Dh</Button>
-          <Button>Ni</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("A")}>Sa</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("B")}>Re</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("C")}>Ga</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("D")}>Ma</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("E")}>Pa</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("F")}>Dh</Button>
+          <Button className="sa" onMouseDown={() => handlePlay("G")}>Ni</Button>
         </Col>
-        <Col md={1}></Col>
+        {/* <Col md={1}></Col> */}
       </Row>
     </div>
   );
