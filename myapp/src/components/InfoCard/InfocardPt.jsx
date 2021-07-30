95f048aa2ae2bb4c90dd28177c0e86c153ce671f
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-30 18:21:54 +0530

    Create InfocardPt.jsx

---
+++ myapp/src/components/InfoCard/InfocardPt.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Card, Col, Row, Form } from "react-bootstrap";
+import "./Infocard.css";
+import { InfocardCh } from "./InfocardCh";
+import { InfocardReducer } from "../../Reducers/InfocardReducer";
+
+export const InfocardPt = () => {
+  const [empArr, setEmpArr] = useState(InfocardReducer.Data);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    const filterArr = empArr.filter(
+      (item) =>
+        item.firstName.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.lastName.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.firstLine.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.secLine.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.cityName.toLowerCase().includes(searchText.toLowerCase())
+    );
+    if (searchText === "") setEmpArr(InfocardReducer.Data);
+    else setEmpArr(filterArr);
+  }, [searchText]);
+  return (
+    <Card className="InfocardPtcard">
+      <h2>Employee List</h2>
+      <Form.Control
+        type="text"
+        onChange={(e) => setSearchText(e.target.value)}
+      ></Form.Control>
+      <Row>
+        {empArr.map((item) => (
+          <Col md={3}>
+            <InfocardCh cardData={item} />
+          </Col>
+        ))}
+      </Row>
+    </Card>
+  );
+};
