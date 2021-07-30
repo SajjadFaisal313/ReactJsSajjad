3ab452e365d45add5c8230cc36d4a7d79230d4d3
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-07-30 18:21:47 +0530

    Create InfocardReducer.js

---
+++ myapp/src/Reducers/InfocardReducer.js
@@ -0,0 +1,60 @@
+export const InfocardReducer = {
+    Data: [
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Amir",
+            lastName: "Saleem",
+            firstLine: "Khair Nagar",
+            secLine: "Bidar Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        },
+        {
+            firstName: "Faisal",
+            lastName: "Ikramuddin",
+            firstLine: "Al-Ameen Nagar",
+            secLine: "Jalkot Road",
+            cityName: "Udgir"
+        }
+    ],
+}
