419d63797831f1a5cb8f997bec2a819f4919abc9
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-01 20:59:06 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
-import { Counter } from './components/Counter';
+// import { Counter } from './components/Counter';
 // import { Audioplayer } from './components/Audioplayer';
 // import { Details } from './components/Details';
 // import { FormTable } from './components/FormTable';
 // import { Toggle } from './components/Toggle';
 // import { Piano } from './components/Piano';
 // import { CourseInfo } from './components/CourseInfo';
-// import { CourseInfoParent } from './components/CourseInfoParent';
+import { CourseInfoParent } from './components/CourseInfoParent';
 // import { InfocardPt } from './components/InfoCard/InfocardPt';
 
 
 function App() {
   return (
     <div className="App">
-      <Counter/>
+      <CourseInfoParent/>
     </div>
   );
 }
