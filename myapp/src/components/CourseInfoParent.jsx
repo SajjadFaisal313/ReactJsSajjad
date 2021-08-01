445234ae743d78decc0c1fe9e4ee54e2a4857ca5
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-02 00:26:35 +0530

    Update CourseInfoParent.jsx

--- myapp/src/components/CourseInfoParent.jsx
+++ myapp/src/components/CourseInfoParent.jsx
@@ -1,87 +1,91 @@
 import React from "react";
+
 // import FormImpl from "react-bootstrap/esm/Form";
-import { CourseInfo } from "./components/CourseInfo";
+import { CourseInfo } from "./CourseInfo";
 
 export const CourseInfoParent = () => {
-  <div className="App">
-    <h1>List Of Institutions</h1>
-    <tr>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Mech)"
-          Institute="SGGS"
-          City="Nanded"
-          Contact="8956487481"
-        />
-      </td>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Civil)"
-          Institute="DY Patil"
-          City="Pune"
-          Contact="9745658986"
-        />
-      </td>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Electrical)"
-          Institute="GECA"
-          City="Aurangabad"
-          Contact="9356446645"
-        />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Mech)"
-          Institute="SGGS"
-          City="Nanded"
-          Contact="8956487481"
-        />
-      </td>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Civil)"
-          Institute="DY Patil"
-          City="Pune"
-          Contact="9745658986"
-        />
-      </td>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Electrical)"
-          Institute="GECA"
-          City="Aurangabad"
-          Contact="9356446645"
-        />
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Mech)"
-          Institute="SGGS"
-          City="Nanded"
-          Contact="8956487481"
-        />
-      </td>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Civil)"
-          Institute="DY Patil"
-          City="Pune"
-          Contact="9745658986"
-        />
-      </td>
-      <td>
-        <CourseInfo
-          CourseName="B.E (Electrical)"
-          Institute="GECA"
-          City="Aurangabad"
-          Contact="9356446645"
-        />
-      </td>
-    </tr>
-  </div>;
+  return (
+    <div>
+      <h1>List Of Institutions</h1>
+      <tr>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Mech)"
+            Institute="SGGS"
+            City="Nanded"
+            Contact="8956487481"
+          />
+        </td>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Civil)"
+            Institute="DY Patil"
+            City="Pune"
+            Contact="9745658986"
+          />
+        </td>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Electrical)"
+            Institute="GECA"
+            City="Aurangabad"
+            Contact="9356446645"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Mech)"
+            Institute="SGGS"
+            City="Nanded"
+            Contact="8956487481"
+          />
+        </td>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Civil)"
+            Institute="DY Patil"
+            City="Pune"
+            Contact="9745658986"
+          />
+        </td>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Electrical)"
+            Institute="GECA"
+            City="Aurangabad"
+            Contact="9356446645"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Mech)"
+            Institute="SGGS"
+            City="Nanded"
+            Contact="8956487481"
+          />
+        </td>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Civil)"
+            Institute="DY Patil"
+            City="Pune"
+            Contact="9745658986"
+          />
+        </td>
+        <td>
+          <CourseInfo
+            CourseName="B.E (Electrical)"
+            Institute="GECA"
+            City="Aurangabad"
+            Contact="9356446645"
+          />
+        </td>
+      </tr>
+      <p>Hello</p>
+    </div>
+  );
 };
