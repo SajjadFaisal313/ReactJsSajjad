d9526c4a52f2bc1f721e8d144d9d2a547a91b009
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-01 20:49:01 +0530

    aug1a

--- myapp/src/components/CourseInfo.jsx
+++ myapp/src/components/CourseInfo.jsx
@@ -1,30 +1,36 @@
-export const CourseInfo=({CourseName, Institute, Contact, City})=>{
-    return(
-     
-        <div>
-          
-        <table border="3px">
+export const CourseInfo = ({ CourseName, Institute, Contact, City }) => {
+  return (
+    <div>
+      <table border="3px">
         <tr>
           <td>CourseName</td>
           <td>:</td>
-          <td><span style={{color:"red"}}>{CourseName}</span></td>
+          <td>
+            <span style={{ color: "red" }}>{CourseName}</span>
+          </td>
         </tr>
         <tr>
           <td>Institute</td>
           <td>:</td>
-          <td><span style={{color:"blue"}}>{Institute} </span></td>
+          <td>
+            <span style={{ color: "blue" }}>{Institute} </span>
+          </td>
         </tr>
         <tr>
           <td>Contact</td>
           <td>:</td>
-          <td><span style={{color:"green"}}>{Contact} </span></td>
+          <td>
+            <span style={{ color: "green" }}>{Contact} </span>
+          </td>
         </tr>
         <tr>
           <td>City</td>
           <td>:</td>
-          <td><span style={{color:"orange"}}>{City}</span></td>
+          <td>
+            <span style={{ color: "orange" }}>{City}</span>
+          </td>
         </tr>
       </table>
-      </div>
-    )
-}
+    </div>
+  );
+};
