dbe4a1f382a5bfb5f96b3e52226cb721ccde8133
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-03 14:31:24 +0530

    router

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,22 +1,32 @@
 import './App.css';
 import React from 'react';
-// import { EmiCal } from './components/Calculator';
-// import { Counter } from './components/Counter';
-// import { Audioplayer } from './components/Audioplayer';
-// import { Details } from './components/Details';
-// import { FormTable } from './components/FormTable';
-// import { Toggle } from './components/Toggle';
-import { MyInfo } from './components/MyInfo';
-// import { Piano } from './components/Piano';
-// import { CourseInfo } from './components/CourseInfo';
-// import { CourseInfoParent } from './components/CourseInfoParent';
-// import { InfocardPt } from './components/InfoCard/InfocardPt';
+
+import { AboutUs } from "./components/Routes/AboutUs";
+import { Contact } from "./components/Routes/Contact";
+import { Courses } from "./components/Routes/Courses";
+import { Home } from "./components/Routes/Home";
+import { Navigation } from "./components/Routes/Navigation/Navigation";
+import { Services } from "./components/Routes/Services";
+import { Login } from "./components/Login"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <MyInfo/>
+         <Router>
+      <Navigation/>
+      <Switch>
+      <div>
+        <Route path="/Home"><Home/></Route>
+        <Route path="/AboutUs"><AboutUs/></Route>
+        <Route path="/Services"><Services/></Route>
+        <Route path="/Contact"><Contact/></Route>
+        <Route path="/Courses"><Courses/></Route>
+        <Route exact path="/"><Login/></Route>
+      </div>
+      </Switch>
+    </Router>
     </div>
   );
 }
