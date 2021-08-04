1f582ba39d0c3c8740be267aee48cec5b2094908
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-04 15:37:31 +0530

    Update Courses.jsx

--- myapp/src/components/Routes/Courses.jsx
+++ myapp/src/components/Routes/Courses.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { userData } from "../config";
 
 export const Courses = () => {
+  const history = useHistory();
+  useEffect(() => {
+    if (userData.token == localStorage.getItem("token"));
+    history.push("/");
+  }, []);
   return <div>this is courses</div>;
 };
