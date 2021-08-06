fe40cb991f8cfd0a0c8f33a9292c8372c0d0361b
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-06 16:22:55 +0530

    Create Pronounciation.jsx

---
+++ myapp/src/components/Dictionary/Pronounciation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export const Pronounciation = () => {
+  return (
+    <div className="proncard">
+      <h3>Pronounciation</h3>
+    </div>
+  );
+};
