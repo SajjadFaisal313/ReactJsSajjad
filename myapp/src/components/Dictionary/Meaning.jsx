769815fcb5969cf6e9cbe792acd592e8a82ca093
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-06 16:22:49 +0530

    Create Meaning.jsx

---
+++ myapp/src/components/Dictionary/Meaning.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export const Meaning = () => {
+  return (
+    <div className="meaningcard">
+      <h4>Meaning</h4>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo expedita
+        reiciendis doloremque. Ipsa nobis commodi delectus architecto illo error
+        ad!
+      </p>
+    </div>
+  );
+};
