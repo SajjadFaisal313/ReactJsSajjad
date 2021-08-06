992266071e328f2836246e36573682f2b4515ef0
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-06 16:23:04 +0530

    Update App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import React from 'react';
 
-import { AboutUs } from "./components/Routes/AboutUs";
-import { Contact } from "./components/Routes/Contact";
-import { Courses } from "./components/Routes/Courses";
-import { Home } from "./components/Routes/Home";
-import { Navigation } from "./components/Routes/Navigation/Navigation";
-import { Services } from "./components/Routes/Services";
-import { Login } from "./components/Login"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Clock } from './components/Clock';
+// import { AboutUs } from "./components/Routes/AboutUs";
+// import { Contact } from "./components/Routes/Contact";
+// import { Courses } from "./components/Routes/Courses";
+// import { Home } from "./components/Routes/Home";
+// import { Navigation } from "./components/Routes/Navigation/Navigation";
+// import { Services } from "./components/Routes/Services";
+// import { Login } from "./components/Login"
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { Clock } from './components/Clock';
+// import { Counter } from './components/Counter';
+import { DictionaryParent } from './components/Dictionary/DictionaryParent';
 
 
 function App() {
@@ -28,7 +30,7 @@ function App() {
       </div>
       </Switch>
     </Router> */}
-    <Clock/>
+    <DictionaryParent/>
     </div>
   );
 }
