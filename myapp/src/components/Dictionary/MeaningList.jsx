50722e9f02d0258819f418910fb5ca6c32b0df86
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-06 17:37:33 +0530

    de

---
+++ myapp/src/components/Dictionary/MeaningList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Meaning } from "./Meaning";
+
+export const MeaningList = ({ item }) => {
+  return (
+    <div className="meaningcard">
+      {item.meanings.map((means) => (
+        <Meaning />
+      ))}
+    </div>
+  );
+};
