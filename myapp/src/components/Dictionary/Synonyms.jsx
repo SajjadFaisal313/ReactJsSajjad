338800d7982b64b7329f3905dc5d2c5f46e015a7
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-06 16:23:00 +0530

    Create Synonyms.jsx

---
+++ myapp/src/components/Dictionary/Synonyms.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export const Synonyms = () => {
+  return (
+    <div className="syncard">
+      <h4>Synonyms</h4>
+      <ol>
+        <li>Meaning1</li>
+        <li>Meaning2</li>
+        <li>Meaning3</li>
+        <li>Meaning4</li>
+      </ol>
+    </div>
+  );
+};
