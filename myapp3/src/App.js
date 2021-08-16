2c8427bd8288fa027aba0ff4dcb859398973902a
SajjadFaisal313 <sajjadfaisalikramuddin@gmail.com>
2021-08-16 19:20:32 +0530

    Update App.js

--- myapp3/src/App.js
+++ myapp3/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-
+import { Home } from "./Components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-
+      <Home />
     </div>
   );
 }
